87eca3aabbdccac265449939905598527fb25aed
Hiroaki Sengoku <sengoku@gcd.org>
2002-10-05 14:43:27 +0000

    Thanks to Mr. Nikola Vladov.

--- stone.c
+++ stone.c
@@ -1341,7 +1341,8 @@ struct sockaddr_in *sinp;	/* connect to */
 		if (Debug > 4)
 		    message(LOG_DEBUG,"TCP %d: connect interrupted",pair->sd);
 		return 0;
-	    } else if (errno == EISCONN || errno == EADDRINUSE) {
+	    } else if (errno == EALREADY
+		       || errno == EISCONN || errno == EADDRINUSE) {
 		if (Debug > 4) {	/* SunOS's bug ? */
 		    message(LOG_INFO,"TCP %d: connect bug err=%d",
 			    pair->sd,errno);
