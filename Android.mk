f400145848f94561d81922a0694c2a1189bbf1a8
hiroaki_sengoku <hiroaki_sengoku>
2013-12-27 04:09:37 +0000

    Android.mk for jellybean USE_TPROXY

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 # export TOP=/usr/local/src/Android/gingerbread
+# export TOP=/usr/local/src/Android/jellybean
 # . $TOP/build/envsetup.sh 
 # mm showcommands
 
@@ -11,6 +12,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := libc libcutils libcrypto libssl
 LOCAL_CFLAGS := -DANDROID -DNO_SYSLOG -DCONST_SSL_METHOD -DTHREAD_UNSAFE -DNO_RINDEX -DPTHREAD -DUNIX_DAEMON -DSO_ORIGINAL_DST=80 -DUSE_POP -DUSE_SSL
 LOCAL_SRC_FILES := stone.c
-LOCAL_C_INCLUDES := external/openssl/include frameworks/base/cmds/keystore
+#LOCAL_C_INCLUDES := external/openssl/include frameworks/base/cmds/keystore
+LOCAL_C_INCLUDES := external/openssl/include system/security/keystore
 
 include $(BUILD_EXECUTABLE)
