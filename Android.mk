ba934d8cfccb41ada6122ff83f239098f362b079
hiroaki_sengoku <hiroaki_sengoku>
2013-12-27 09:01:58 +0000

    for kitkat android-4.4_r1

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
-# export TOP=/usr/local/src/Android/gingerbread
-# export TOP=/usr/local/src/Android/jellybean
+# export ANDROID_JAVA_HOME=/usr/lib/jvm/java-6-openjdk
+# export TOP=/usr/local/src/android-4.4_r1
 # . $TOP/build/envsetup.sh 
 # mm showcommands
 
@@ -9,10 +9,9 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := stone
 LOCAL_MODULE_TAGS := optional
-LOCAL_SHARED_LIBRARIES := libc libcutils libcrypto libssl
+LOCAL_SHARED_LIBRARIES := libc libcutils libcrypto libssl libkeystore_binder
 LOCAL_CFLAGS := -DANDROID -DNO_SYSLOG -DCONST_SSL_METHOD -DTHREAD_UNSAFE -DNO_RINDEX -DPTHREAD -DUNIX_DAEMON -DSO_ORIGINAL_DST=80 -DUSE_POP -DUSE_SSL
 LOCAL_SRC_FILES := stone.c
-#LOCAL_C_INCLUDES := external/openssl/include frameworks/base/cmds/keystore
-LOCAL_C_INCLUDES := external/openssl/include system/security/keystore
+LOCAL_C_INCLUDES := external/openssl/include frameworks/base/cmds/keystore system/security/keystore/include/keystore
 
 include $(BUILD_EXECUTABLE)
