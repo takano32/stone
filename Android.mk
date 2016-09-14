046509588d3e6cc5e5c62f2a4c6b9fede56fe0db
hiroaki_sengoku <hiroaki_sengoku>
2016-09-14 08:59:15 +0000

    using BoringSSL in android-7

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
-# export ANDROID_JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-# export TOP=/usr/local/src/android-6.0.0_r1
+# export ANDROID_JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+# export TOP=/usr/local/src/android-7.0.0_r6
 # . $TOP/build/envsetup.sh 
 # mm showcommands
 
@@ -10,8 +10,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := stone
 LOCAL_MODULE_TAGS := optional
 LOCAL_SHARED_LIBRARIES := libc libcutils libcrypto libssl libkeystore_binder
-LOCAL_CFLAGS := -DANDROID -DNO_SYSLOG -DCONST_SSL_METHOD -DTHREAD_UNSAFE -DNO_RINDEX -DPTHREAD -DUNIX_DAEMON -DSO_ORIGINAL_DST=80 -DUSE_POP -DUSE_SSL
+LOCAL_CFLAGS := -DANDROID -DNO_SYSLOG -DCONST_SSL_METHOD -DTHREAD_UNSAFE -DNO_RINDEX -DNO_BCOPY -DPTHREAD -DUNIX_DAEMON -DSO_ORIGINAL_DST=80 -DUSE_POP -DUSE_SSL -DOPENSSL_NO_SSL2 -DOPENSSL_NO_SSL3
 LOCAL_SRC_FILES := stone.c
-LOCAL_C_INCLUDES := external/openssl/include frameworks/base/cmds/keystore system/security/keystore/include/keystore
+LOCAL_C_INCLUDES := external/openssl/include external/boringssl/include frameworks/base/cmds/keystore system/security/keystore/include/keystore
 
 include $(BUILD_EXECUTABLE)
